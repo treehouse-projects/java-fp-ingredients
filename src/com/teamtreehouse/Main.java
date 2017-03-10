b8f0826be7508080cffa709315cf9c9294f0261c
Craig Dennis <craig@teamtreehouse.com>
2017-03-09 18:05:24 -0800

    Adds use of supplier

--- src/com/teamtreehouse/Main.java
+++ src/com/teamtreehouse/Main.java
@@ -2,16 +2,23 @@ package com.teamtreehouse;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 import java.util.function.Consumer;
 import javafx.application.Application;
 
 public class Main {
 
     public static void yell(String words) {
+      Objects.requireNonNull(words, () -> "Created issue:  " + Main.createIssue());
       System.out.printf("%s!!!!! %n", words.toUpperCase());
     }
 
-    public static void main(String[] args) {
+  private static String createIssue() {
+    System.out.println("Some external API call to a bug tracker is made and a new issue created");
+    return "#ABC123";
+  }
+
+  public static void main(String[] args) {
       List<String> ingredients = Arrays.asList(
           "flour",
           "salt",
@@ -25,6 +32,7 @@ public class Main {
 //      ingredients.forEach(System.out::println);
       ingredients.forEach(Main::yell);
 
+      Main.yell(null);
 
     }
 }
